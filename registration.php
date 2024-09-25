8edfe5473715c992f6acc3c79f2be66bba35019c
Jerke Combee <jerke1988@gmail.com>
2024-09-25 20:22:07 +0200

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Copyright Jerke Combee. All rights reserved.
+ * https://www.jcombee.nl/
+ */
+
+\MageEye\MagentoDisguisedComponentRegistrar\Component\ComponentRegistrar::removeDisguise(
+    \MageEye\MagentoDisguisedComponentRegistrar\Component\ComponentRegistrar::MODULE,
+    'MageEye_MagentoRecorder'
+);
